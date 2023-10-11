ee5a6e9e6ff6e9f39de6eebc64df83ab76fb921e
Cristal Davila <cristal.davila@gmail.com>
2023-10-11 12:11:09 -0600

    in progress

--- server/app.py
+++ server/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, make_response
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from flask_restful import API, Resource
+from models import Apartment, Tenant, Lease
 
-from models import db
+
+from models import db, Tenant
+
+#import ipdb
 
 app = Flask( __name__ )
 app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///apartments.db'
@@ -14,3 +18,126 @@ db.init_app( app )
 
 if __name__ == '__main__':
     app.run( port = 3000, debug = True )
+
+
+@app.route('/apartments/<int:id>', methods=['GET','POST', 'DELETE'])
+def apartment_by_id(id):
+    apartment = Apartment.query.filter(Apartment.id == id).first()
+
+    if apartment.method == 'GET':
+        apartment_dict = apartment.to_dict()
+
+        response = make_response(
+            apartment_dict,
+            200
+        )
+
+        return response
+
+    elif apartment.method == 'DELETE':
+        db.session.delete(apartment)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "apartment deleted."    
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+    
+    elif apartment.method == 'POST':
+        db.session.post(apartment)
+        db.session.commit()
+
+        response_body = {
+            "post_successful": True,
+            "message": "apartment posted"
+        }
+
+        response = make_response(
+            response_body,
+            200
+
+        )
+
+@app.route('/tenant/<int:id>', methods= ['GET', 'POST', 'DELETE'])
+def tentant_by_id(id):
+    tenant = Tenant.query.filter(Tenant.id == id).first()
+
+    if tenant.method == 'GET':
+        tenant_dict = tenant.to_dict()
+
+        response = make_response(
+            tenant_dict,
+            200
+        )
+
+        return response
+
+    elif tenant.method == 'DELETE':
+        db.session.delete(tenant)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "tenant deleted."    
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+    
+    elif tenant.method == 'POST':
+        db.session.post(tenant)
+        db.session.commit()
+
+        response_body = {
+            "post_successful": True,
+            "message": "tenant posted"
+        }
+
+        response = make_response(
+            response_body,
+            200
+
+        )
+           
+@app.route('/lease/<int:id>', methods= ['POST', 'DELETE'])
+def lease_by_id(id):
+    lease = Lease.query.filter(Lease.id == id).first()
+
+    if lease.method == 'DELETE':
+        db.session.delete(lease)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "lease deleted"    
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+        return response
+
+    
+    elif lease.method == 'POST':
+        db.session.post(lease)
+        db.session.commit()
+
+        response_body = {
+            "post_successful": True,
+            "message": "lease posted"
+        }
+
+        response = make_response(
+            response_body,
+            200
+
+        )
+           
--- server/models.py
+++ server/models.py
@@ -5,3 +5,45 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
+
+
+class Tenant(db.model):
+    __tablename__ = 'tenant'
+
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    
+apartments = db.relationship(
+    'Apartment',back_populates='tenants')
+
+def __repr__(self):
+    return f'<Tenant {self.id}, {self.name}, {self.age}>'
+
+class Lease(db.model):
+    __tablename__ = 'lease'
+
+    id = db.Column(db.Integer, primary_key= True)
+    rent = db.Column(db.Float)
+
+    #foreign keys for tenant and apartment
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenant.id'))
+    apartment_id = db.Column(db.Integer, db.ForeignKey('apartment.id'))
+    
+    #establish relationships between databases
+    tenant = db.relationship('tenant', back_populates='lease')
+    apartment = db.relationship('apartment', back_populates='lease')
+    
+
+class Apartment(db.model):
+    __tablename__ = 'apartment'
+
+    id = db.Column(db.Integer, primary_key= True)
+    number = db.Column(db.Integer)
+
+tenants = db.relationship(
+    'Tenant', back_populates='apartments')
+   
+def __repr__(self):
+    return f'<Apartment {self.id}, {self.number}>'
+
